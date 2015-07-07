57ef4df1b824d5ea7ed35213a5f6d0ae0d57bd7a
Ming Fang <mindbergh@gmail.com>
2015-07-07 11:46:24 -0400

    remove case id

--- weiss/tests/test_dialogue.py
+++ weiss/tests/test_dialogue.py
@@ -8,10 +8,9 @@ from webapps.settings import BASE_DIR
 class Case(object):
 
     def __init__(self, line):
-        self.ID = int(line[0])
-        self.query = line[1]
-        self.curr_sid = int(line[2])
-        self.expected = int(line[3])
+        self.query = line[0]
+        self.curr_sid = int(line[1])
+        self.expected = int(line[2])
         self.classifier = getClassifier()
         self.fmgr = getFlowManager()
         self.curr_state = self.fmgr.lookUp(self.curr_sid)
@@ -24,9 +23,13 @@ class Case(object):
 
     def __str__(self):
         if self.check():
-            return PASSED + " ID: %s, query: %s, curr_state: %s, expected: %s, actual: %s" % (self.ID, self.query, self.curr_state, self.expected, self.actual)
+            return PASSED + \
+                    " query: %s, curr_state: %s, expected: %s, actual: %s" % \
+                    (self.query, self.curr_state, self.expected, self.actual)
         else:
-            return FAILED + " ID: %s, query: %s, curr_state: %s, expected: %s, actual: %s" % (self.ID, self.query, self.curr_state, self.expected, self.actual)
+            return FAILED + \
+                    " query: %s, curr_state: %s, expected: %s, actual: %s" % \
+                    (self.query, self.curr_state, self.expected, self.actual)
 
 
     def check(self):
