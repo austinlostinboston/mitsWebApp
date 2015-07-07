da320f1f21cea40830c5b838a758ace0b26cd9ae
Ming Fang <mindbergh@gmail.com>
2015-07-07 15:38:38 -0400

    change comment

--- weiss/classifier/models/link.sh
+++ weiss/classifier/models/link.sh
@@ -1,14 +1,12 @@
-: '
-This file will link file named action_model and action_features
-which are in the same directory as this one to the feature file
-model file under the directory of /home/wenjunw/Weiss/classifier/models/
-Feature and model files under the destination directory is updated
-everyday.
-
-Author: Wenjun Wang<wenjunw@cs.cmu.edu>
-Date: July 1, 2015
-'
 #!/bin/bash
+#This file will link file named action_model and action_features
+#which are in the same directory as this one to the feature file
+#model file under the directory of /home/wenjunw/Weiss/classifier/models/
+#Feature and model files under the destination directory is updated
+#everyday.
+#
+#Author: Wenjun Wang<wenjunw@cs.cmu.edu>
+#Date: July 1, 2015
 date=$(date +%Y-%m-%d)
 DIR=/home/wenjunw/Weiss/classifier/models/
 modelname=$DIR"model_"$date
