1147781292b32177e0733be134f8c83101f0abbf
Ming Fang <mindbergh@gmail.com>
2015-07-21 10:17:55 -0400

    add comment

--- weiss/flows/flowManager.py
+++ weiss/flows/flowManager.py
@@ -1,12 +1,11 @@
 """
-Flow Mnager
+Flow Manager
 
-NOTE: this class should be created by signleton factory in factory.py
+NOTE: this class should be created by singleton factory in factory.py
 
 this class is responsible for
-    1. user state factory.
-    2. loop up state by sid
-    3. make transit from one state based on a given action
+    1. register a user's flow
+    2. loop up flow by user id
 
 Author: Ming Fang <mingf@cs.cmu.edu>
 """
@@ -21,11 +20,19 @@ class FlowManager:
         self._flowTable = {}
 
     def register(self, uid, flow):
+        """Register a user's flow object
+
+        :param uid: the user id
+        :param flow: the flow obj associated with the user
+        :return: void
+        """
         self._flowTable[uid] = flow
         return
 
     def lookUp(self, uid):
-        if not self._flowTable.has_key(uid):
-            return None
-        else:
-            return self._flowTable[uid]
+        """Look up a flow given a user id
+
+        :param uid: the user id to be looked up, get it by request.user
+        :return: the flow obj associated with the given user, or None if not found
+        """
+        return self._flowTable.get(uid, None)
